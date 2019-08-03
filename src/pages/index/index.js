7020efd1b2267b3354bd7bfe91b8216ca6bc6e77
jenvyxu <1272117264@qq.com>
2019-08-03 20:02:01 +0800

    实现首页数据列表获取

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,15 +1,48 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'css/common.css'
+import './index.css'
+import { InfiniteScroll } from 'mint-ui';
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import axios from 'axios'
+import url from 'js/api.js'
 
-Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(InfiniteScroll);
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el:'#app',
+    data:{
+        lists:null,
+        pageNum:1,
+        pageSize:6,
+        loading:false,
+        allLoaded:false
+    },
+    created(){
+        this.getLists()
+    },
+    methods:{
+        getLists(){
+            if(this.allLoaded)return //如果所有数据加载完毕，不再执行下去
+            this.loading=true //加载数据时防止反复请求数据
+            axios.post(url.hotLists,{
+                pageNum:this.pageNum,
+                pageSize:this.pageSize
+            }).then(res=>{
+                let curLists=res.data.lists
+                if(curLists.length<this.pageSize){
+                    //判断是否所有数据都已经加载完毕
+                    this.allLoaded=true
+                }
+
+                if(this.lists){
+                    this.lists=this.lists.concat(curLists)
+                }else{
+                    //第一次请求数据
+                    this.lists=curLists
+                }
+                this.loading=false
+                this.pageNum++   
+            })          
+        }
+    }
 })
