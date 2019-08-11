bc08f699cd794001ac0210dee8b084a5cf3d12b9
jenvyxu <1272117264@qq.com>
2019-08-11 22:05:31 +0800

    添加编辑和展示商品切换

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -8,64 +8,109 @@ import axios from 'axios'
 import url from 'js/api.js'
 
 new Vue({
-    el:'.container',
-    data:{
-        lists:null,
+    el: '.container',
+    data: {
+        lists: null,
+        total: 0,
+        totalNum:0,
+        editingShop:null,
+        editingShopIndex:-1
     },
-    created(){
+    created() {
         this.getLists()
     },
-    methods:{
-        getLists(){
-            axios.post(url.cartLists).then(res=>{
-                
+    methods: {
+        getLists() {
+            axios.post(url.cartLists).then(res => {
+
                 //先赋值后添加属性无法实现响应式
-                let lists=res.data.cartLists
-                lists.forEach(shop=>{
-                    shop.checked=true
-                    shop.goodsList.forEach(good=>{
-                        good.checked=true
+                let lists = res.data.cartLists
+                lists.forEach(shop => {
+                    shop.checked = true
+                    shop.removeChecked=false
+                    shop.editing=false
+                    shop.editingMsg='编辑'
+                    shop.goodsList.forEach(good => {
+                        good.checked = true
+                        good.removeChecked=false
                     })
                 })
                 //先处理后赋值
-                this.lists=lists
+                this.lists = lists
             })
         },
 
-        selectGood(shop,good){
-            good.checked=!good.checked
-            shop.checked=shop.goodsList.every(good=>{
+        selectGood(shop, good) {
+            good.checked = !good.checked
+            shop.checked = shop.goodsList.every(good => {
                 return good.checked
             })
         },
-        selectedShop(shop){
-            shop.checked=!shop.checked
-            shop.goodsList.forEach(good=>{
-                good.checked=shop.checked
+        selectedShop(shop) {
+            shop.checked = !shop.checked
+            shop.goodsList.forEach(good => {
+                good.checked = shop.checked
             })
         },
-        selectAll(){
-            this.allSelected=!this.allSelected
+        selectAll() {
+            this.allSelected = !this.allSelected
+        },
+        edit(shop,shopIndex){
+            shop.editing=!shop.editing
+            shop.editingMsg=shop.editing?'完成':'编辑'
+            this.lists.forEach((item,i)=>{
+                if(shopIndex!==i){
+                    item.editing=false
+                    item.editingMsg=shop.editing?'':'编辑'
+                }
+            })
+            this.editingShop=shop.editing?shop:null
+            this.editingShopIndex=shop.editing?shopIndex:-1
+
         }
     },
-    computed:{
-        allSelected:{
-            get(){
-                if(this.lists&&this.lists.length){
-                    return this.lists.every(shop=>{
+    computed: {
+        allSelected: {
+            get() {
+                if (this.lists && this.lists.length) {
+                    return this.lists.every(shop => {
                         return shop.checked
                     })
                 }
                 return false
             },
-            set(newVal){
-                this.lists.forEach(shop=>{
-                    shop.checked=newVal
-                    shop.goodsList.forEach(good=>{
-                        good.checked=newVal
+            set(newVal) {
+                this.lists.forEach(shop => {
+                    shop.checked = newVal
+                    shop.goodsList.forEach(good => {
+                        good.checked = newVal
                     })
                 })
             }
+        },
+        selectLists() {
+            if (this.lists && this.lists.length) {
+                let arr = []
+                let total = 0
+                let totalNum=0
+                this.lists.forEach(shop => {
+                    shop.goodsList.forEach(good => {
+                        if(good.checked){
+                            arr.push(good)
+                            total+=good.price*good.number
+                            totalNum+=1
+                        }
+                    })
+                })
+                this.total=total
+                this.totalNum=totalNum
+                return arr
+            }
+            return []
+        },
+        removeLists(){
+
         }
-    }
+    },
+    mixins:[mixin]
 })
